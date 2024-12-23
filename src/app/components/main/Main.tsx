4c637fad6934a4f04c7dee2f9179d27108e08f3f
minpppal <minpal203@gmail.com>
2024-12-23 23:30:16 +0900

    feat: 카카오 로그인 버튼 이미지(#27)

---
+++ src/app/components/main/Main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import KakaoLoginButton from "./kakaoLoginButton/KakaoLoginButton";
+const Main = () => {
+  return (
+    <div>
+      <KakaoLoginButton />
+    </div>
+  );
+};
+
+export default Main;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Main from "./components/main/Main";
+
 export default function Home() {
   return (
     <>
       <div className="text-[60px] w-[1000px] h-screen bg-gray-300 flex justify-start items-center flex-col ">
-        <div>Component render</div>
+        <div>
+          <Main />
+        </div>
       </div>
     </>
   );
