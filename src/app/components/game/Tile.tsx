399b1843274fd031fc7b6c8a3ebd89188d232834
minpppal <minpal203@gmail.com>
2025-01-04 22:46:35 +0900

    feat: 게임 CSS 구현(#34)

--- src/app/components/game/Tile.tsx
+++ src/app/components/game/Tile.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const Tile = () => {
+interface TileProps {
+  number: number | null;
+}
+
+const Tile = ({ number }: TileProps) => {
   return (
-    <div className="flex items-center justify-center gap-2 w-14 h-14 bg-amber-100 text-amber-800 rounded-lg">
-      2
+    <div className="flex items-center justify-center  w-[96px] h-[96px] bg-amber-100 text-amber-800 rounded-lg text-[24px] font-bold shadow-lg">
+      {number !== null ? number : ""}
     </div>
   );
 };
