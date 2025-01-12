e0e7c2fd9d028ecd595b825734e02636fd461932
minpppal <minpal203@gmail.com>
2025-01-12 13:58:02 +0900

    fix: 풀리퀘스트 yml파일 제목 수정 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Main from "./components/main/Main";
 
 export default function Home() {
-  //풀리퀘스트 제목 Test2
   return (
     <>
       <div className="text-[60px] w-[1000px] h-screen bg-gradient-to-br from-amber-50 to-orange-100 flex justify-start items-center flex-col ">
