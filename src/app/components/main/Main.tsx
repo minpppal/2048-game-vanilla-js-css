33d57a3d4fa1e174ef3c0107b166d9c9be6d152f
minpppal <minpal203@gmail.com>
2025-01-22 21:27:39 +0900

    style: 메인화면, 숫자타일  CSS 구현

--- src/app/components/main/Main.tsx
+++ src/app/components/main/Main.tsx
@@ -13,17 +13,47 @@ const Main = () => {
     setClickButton(button);
   };
   return (
-    <div>
-      {clickButton === null && (
-        <>
-          <GameStartButton onClick={() => handleButtonClick("gameStart")} />
-          <RankButton onClick={() => handleButtonClick("rank")} />
-        </>
-      )}
-      {/* 상태에 맞게 클릭된 버튼에 해당하는 컴포넌트만 렌더링 */}
-      {clickButton === "gameStart" && <Game />}
-      {clickButton === "rank" && <Rank />}
-    </div>
+    <>
+      <div className="mb-[100px]">
+        <h1 className="text-center font-extrabold text-orange-600 pt-[100px]">
+          2048
+        </h1>
+        <span className="flex text-center text-xl text-orange-800 items-center justify-center">
+          Join the numbers and get to the 2048 tile!
+        </span>
+      </div>
+      <div>
+        {clickButton === null && (
+          <>
+            <div className="flex  flex-col items-center mb-[50px]">
+              <GameStartButton onClick={() => handleButtonClick("gameStart")} />
+              <RankButton onClick={() => handleButtonClick("rank")} />
+            </div>
+            <div>
+              <div className="flex items-center justify-center mb-[15px]">
+                <div className="w-20 h-20 flex items-center justify-center rounded-lg shadow-md text-2xl font-extrabold bg-amber-100 text-amber-800 mr-[15px]">
+                  2
+                </div>
+                <div className="w-20 h-20 flex items-center justify-center rounded-lg shadow-md text-2xl font-extrabold bg-amber-200 text-amber-800">
+                  4
+                </div>
+              </div>
+              <div className="flex items-center justify-center">
+                <div className="w-20 h-20 flex items-center justify-center rounded-lg shadow-md text-2xl font-extrabold bg-orange-300 text-white mr-[15px]">
+                  8
+                </div>
+                <div className="w-20 h-20 flex items-center justify-center rounded-lg shadow-md text-2xl font-extrabold bg-orange-400 text-white">
+                  16
+                </div>
+              </div>
+            </div>
+          </>
+        )}
+        {/* 상태에 맞게 클릭된 버튼에 해당하는 컴포넌트만 렌더링 */}
+        {clickButton === "gameStart" && <Game />}
+        {clickButton === "rank" && <Rank />}
+      </div>
+    </>
   );
 };
 
--- src/app/components/main/gameStartButton/GameStartButton.tsx
+++ src/app/components/main/gameStartButton/GameStartButton.tsx
@@ -7,13 +7,13 @@ interface GameStartButtonProps {
 
 const GameStartButton: React.FC<GameStartButtonProps> = ({ onClick }) => {
   return (
-    <div className="relative">
-      <div className="absolute inset-0 h-16 w-full bg-orange-300 rounded-lg transform rotate-3 z-0 mt-5"></div>
+    <div className="relative ">
+      <div className="absolute inset-0 h-[48px] w-[170px] bg-orange-300 rounded-lg transform rotate-3 z-0 mt-8"></div>
       <button
-        className="relative h-16 px-8 text-xl font-semibold bg-orange-500 hover:bg-orange-600 text-white rounded-lg shadow-lg transition-transform duration-200 ease-in-out transform hover:-translate-y-1 z-10"
+        className="relative h-[48px] w-[170px] px-8 text-xl font-semibold bg-orange-500 hover:bg-orange-600 text-white rounded-lg shadow-lg transition-transform duration-200 ease-in-out transform hover:-translate-y-1 z-10"
         onClick={onClick} // 클릭 시 onClick prop 호출
       >
-        게임 시작
+        Start Game
       </button>
     </div>
   );
